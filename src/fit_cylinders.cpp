f46cff43ba41a7f3447512fa3328bca0078ed1e7
Daniel <sanchez@acsl.co.jp>
2023-07-28 10:49:46 +0900

    [feat] draw origin method

--- src/fit_cylinders.cpp
+++ src/fit_cylinders.cpp
@@ -5,72 +5,25 @@
 
 #include "cylinder_fitting.hpp"
 
-double projection_of_point_in_axis(const Eigen::Vector3d& direction,
-                                   const Eigen::Vector3d& line_point,
-                                   const Eigen::Vector3d& point) {
-  auto line_point_to_point = point - line_point;
-  return line_point_to_point.dot(direction);
-}
+void draw_origin(pcl::visualization::PCLVisualizer& viewer){
+// Create the coordinate frame manually
+  pcl::PointXYZ origin(0, 0, 0);
+  pcl::PointXYZ x_axis(1, 0, 0);
+  pcl::PointXYZ y_axis(0, 1, 0);
+  pcl::PointXYZ z_axis(0, 0, 1);
 
-template <typename PointT>
-void adjust_cylinder_model_to_points(
-    typename pcl::PointCloud<PointT>::Ptr& cylinder_cloud,
-    Eigen::VectorXd& cylinder_coefficients) {
-  // Extract the cylinder parameters from the x vector
-  Eigen::Vector3d axis_ = cylinder_coefficients.segment<3>(0);
-  Eigen::Vector3d center_ = cylinder_coefficients.segment<3>(3);
+  viewer.addLine(origin, x_axis, 1.0, 0.0, 0.0, "x_axis");
+  viewer.addLine(origin, y_axis, 0.0, 1.0, 0.0, "y_axis");
+  viewer.addLine(origin, z_axis, 0.0, 0.0, 1.0, "z_axis");
 
-  axis_.normalize();
-  double max_projection = std::numeric_limits<double>::lowest();
-  double min_projection = std::numeric_limits<double>::max();
-  int c = 0;
-  for (auto point : cylinder_cloud->points) {
-    Eigen::Vector3d point_{point.x, point.y, point.z};
-    double projection = projection_of_point_in_axis(axis_, center_, point_);
-    max_projection =
-        (projection > max_projection) ? projection : max_projection;
-    min_projection =
-        (projection < min_projection) ? projection : min_projection;
-  }
-  Eigen::Vector3d new_center_ = center_ + axis_ * min_projection;
-  Eigen::Vector3d new_axis_ = axis_ * (max_projection - min_projection);
-  cylinder_coefficients.segment<3>(0) = new_axis_;
-  cylinder_coefficients.segment<3>(3) = new_center_;
-}
-
-template <typename PointT>
-typename pcl::PointCloud<PointT>::Ptr filter_cylinder_inliers(
-    typename pcl::PointCloud<PointT>::Ptr& cylinder_cloud,
-    Eigen::VectorXd& cylinder_coefficients, double radius_percentage = 0.05) {
-  // Extract the cylinder parameters from the x vector
-  Eigen::Vector3d axis_ = cylinder_coefficients.segment<3>(0);
-  axis_.normalize();
-  Eigen::Vector3d center_ = cylinder_coefficients.segment<3>(3);
-  double radius = cylinder_coefficients(6);
-
-  typename pcl::PointCloud<PointT>::Ptr filtered_cylinder_cloud(
-      new pcl::PointCloud<PointT>);
-
-  filtered_cylinder_cloud->points.resize(cylinder_cloud->points.size());
-  int point_count = 0;
-  for (auto point : cylinder_cloud->points) {
-    Eigen::Vector3d point_{point.x, point.y, point.z};
-    double projection = projection_of_point_in_axis(axis_, center_, point_);
-    double distance_to_axis = (point_ - (center_ + projection * axis_)).norm();
-
-    if (std::abs(distance_to_axis - radius) < radius * radius_percentage)
-      filtered_cylinder_cloud->points[point_count++] = point;
-  }
-  filtered_cylinder_cloud->resize(point_count);
-  return filtered_cylinder_cloud;
 }
 
 int main() {
   pcl::PointCloud<pcl::PointXYZ>::Ptr cylinder_cloud(
       new pcl::PointCloud<pcl::PointXYZ>);
-  Eigen::Vector3d axis(1, 0, 0);
+  Eigen::Vector3d axis(1, 0, 1);
   axis.normalize();
-  Eigen::Vector3d center(0, 0, 0);
+  Eigen::Vector3d center(0, 3, 0);
   double radius = 2;
   double height = 2;
   int n_points = 1000;
@@ -125,15 +78,7 @@ int main() {
   viewer.setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_OPACITY,
                                      transparency, "cylinder");
 
-  // Create the coordinate frame manually
-  pcl::PointXYZ origin(0, 0, 0);
-  pcl::PointXYZ x_axis(1, 0, 0);
-  pcl::PointXYZ y_axis(0, 1, 0);
-  pcl::PointXYZ z_axis(0, 0, 1);
-
-  viewer.addLine(origin, x_axis, 1.0, 0.0, 0.0, "x_axis");
-  viewer.addLine(origin, y_axis, 0.0, 1.0, 0.0, "y_axis");
-  viewer.addLine(origin, z_axis, 0.0, 0.0, 1.0, "z_axis");
+  draw_origin(viewer);
 
   viewer.spin();
 
