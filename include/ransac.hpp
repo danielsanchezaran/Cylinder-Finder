5adac1c022bfe7717481f5f5572a2217c4d1ec93
Daniel <sanchez@acsl.co.jp>
2023-08-03 10:43:59 +0900

    [feat] ransac circle w/ test

--- include/ransac.hpp
+++ include/ransac.hpp
@@ -3,7 +3,9 @@
 #include <pcl/point_cloud.h>
 
 #include <Eigen/Core>
+#include <random>
 #include <vector>
+
 
 template <typename PointT>
 inline const Eigen::MatrixXd pcl_to_eigen(
@@ -16,5 +18,84 @@ inline const Eigen::MatrixXd pcl_to_eigen(
   }
   return out_cloud;
 }
+
+inline const Eigen::Vector3d circle_ransac(
+    const Eigen::MatrixXd& data_points, int max_iterations,
+    double threshold_distance, std::vector<int>& inlier_indices,
+    const double max_inlier_ratio = 0.95) {
+  int num_points =
+      data_points.cols();  // Assuming each column is a 2D data point
+
+  if (num_points < 3) {
+    throw std::invalid_argument("At least 3 data points are required.");
+  }
+
+  std::random_device rd;
+  std::mt19937 generator(rd());
+
+  Eigen::Vector3d best_circle;  // (center_x, center_y, radius)
+  int best_inliers_count = 0;
+
+  for (int iteration = 0; iteration < max_iterations; iteration++) {
+    // Randomly select 3 data points to form a circle
+    std::uniform_int_distribution<int> distribution(0, num_points - 1);
+    int idx1 = distribution(generator);
+    int idx2 = distribution(generator);
+    int idx3 = distribution(generator);
+
+    Eigen::Vector2d p1 = data_points.col(idx1);
+    Eigen::Vector2d p2 = data_points.col(idx2);
+    Eigen::Vector2d p3 = data_points.col(idx3);
+
+    // Calculate the circumcenter of the triangle formed by the selected points
+    double x1 = p1[0];
+    double y1 = p1[1];
+    double x2 = p2[0];
+    double y2 = p2[1];
+    double x3 = p3[0];
+    double y3 = p3[1];
+
+    double D = 2.0 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
+    if (std::abs(D) < 1e-9) continue;
+    double Ux =
+        ((x1 * x1 + y1 * y1) * (y2 - y3) + (x2 * x2 + y2 * y2) * (y3 - y1) +
+         (x3 * x3 + y3 * y3) * (y1 - y2)) /
+        D;
+    double Uy =
+        ((x1 * x1 + y1 * y1) * (x3 - x2) + (x2 * x2 + y2 * y2) * (x1 - x3) +
+         (x3 * x3 + y3 * y3) * (x2 - x1)) /
+        D;
+
+    Eigen::Vector2d intersection(Ux, Uy);
+
+    // Calculate the circle radius as the distance between the circumcenter and
+    // any of the points
+    double radius = (p1 - intersection).norm();
+
+    // Count inliers (data points close enough to the circle)
+    std::vector<int> inliers;
+    for (int i = 0; i < num_points; i++) {
+      Eigen::Vector2d p = data_points.col(i);
+      double distance = (p - intersection).norm();
+
+      if (std::abs(distance - radius) < threshold_distance) {
+        inliers.push_back(i);
+      }
+    }
+
+    int inliers_count = inliers.size();
+
+    // Check if this circle model is better than the current best model
+    if (inliers_count > best_inliers_count) {
+      best_inliers_count = inliers_count;
+      best_circle << intersection, radius;
+      inlier_indices = inliers;
+    }
+    if(inliers_count > num_points *  max_inlier_ratio) break;
+  }
+
+  return best_circle;
+}
+
 
 #endif  // RANSAC_HPP
---
+++ tests/ransac_test.cpp
@@ -0,0 +1,71 @@
+#include "ransac.hpp"
+
+#include <gtest/gtest.h>
+
+#include <Eigen/Dense>
+#include <cmath>
+
+// Include the circle_ransac function here
+
+TEST(CircleRANSACTest, BasicTest) {
+  const double center_x = -2.0;
+  const double center_y = -10.0;
+  const double radius = 5.82;
+  const int num_points = 100;
+
+  Eigen::MatrixXd data_points(2, num_points);
+  for (int i = 0; i < num_points; i++) {
+    double angle =
+        2.0 * M_PI * static_cast<double>(i) / static_cast<double>(num_points);
+    double x = center_x + radius * std::cos(angle);
+    double y = center_y + radius * std::sin(angle);
+
+    // Add some random noise to the points to simulate noisy data
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    std::normal_distribution<double> noise_distribution(0.0, 0.1);
+    x += noise_distribution(generator);
+    y += noise_distribution(generator);
+
+    data_points.col(i) << x, y;
+  }
+
+  // Modify the data points to be centered around (0, 0)
+  data_points.row(0).array() -= center_x;
+  data_points.row(1).array() -= center_y;
+
+  std::vector<int> inlier_indices;
+  Eigen::Vector3d result =
+      circle_ransac(data_points, 1000, 0.1, inlier_indices);
+
+  // The correct center should be (0.0, 0.0), and the radius should be 2.0
+  // (approximately).
+  EXPECT_NEAR(result[0], 0.0, 0.1);
+  EXPECT_NEAR(result[1], 0.0, 0.1);
+  EXPECT_NEAR(result[2], radius, 0.1);
+
+  // Verify that the inliers correspond to the points that are close enough to
+  // the circle.
+  Eigen::Vector2d ransac_center(result[0],result[1]);
+  for (int index : inlier_indices) {
+    Eigen::Vector2d p = data_points.col(index);
+    double distance = (ransac_center - p).norm();
+    EXPECT_NEAR(std::abs(distance - result[2]), 0.0, 0.15);
+  }
+}
+
+TEST(CircleRANSACTest, FewDataPointsTest) {
+  Eigen::MatrixXd data_points(2, 2);
+  data_points << 0.0, 1.0, 0.0, 1.0;
+  std::vector<int> inlier_indices;
+  // The function should throw an exception for less than 3 data points.
+  EXPECT_THROW(circle_ransac(data_points, 1000, 0.1, inlier_indices),
+               std::invalid_argument);
+}
+
+// Add more tests if needed
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
