54336cdeba01168c83b785779d36536f48c14efc
Daniel <sanchez@acsl.co.jp>
2023-08-04 10:59:00 +0900

    [feat] added ThreadPools as submodule

---
+++ src/multiple_cylinder_fit.cpp
@@ -0,0 +1,16 @@
+#include <pcl/visualization/pcl_visualizer.h>
+
+#include <chrono>
+#include <iostream>
+#include <limits>
+#include <string>
+
+#include "cylinder_fitting.hpp"
+#include "ransac.hpp"
+#include "thread_pool.hpp"
+
+
+int main(){
+    ThreadPool thread_pool(4);
+    return 0;
+}
